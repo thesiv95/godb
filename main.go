0441c54340ba157ee6b3f9b0309a083bb219f0b6
Ilya Soloveychik <dj.active.17@gmail.com>
2021-03-17 22:31:35 +0200

    implement CRUD methods to db

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -39,8 +40,8 @@ func main() {
 	db, err = sql.Open("postgres", pgUrl)
 	logFatal(err)
 
-	err = db.Ping() // output only when error occurs
-	logFatal(err)
+	err = db.Ping()
+	logFatal(err) // output will be shown only when error occurs
 
 	router := mux.NewRouter()
 
@@ -52,13 +53,56 @@ func main() {
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
+	var user User
+	users = []User{}
+
+	rows, err := db.Query("select * from users")
+	logFatal(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&user.UserId, &user.Name, &user.Lastname, &user.Birthdate)
+		logFatal(err) // output will be shown only when error occurs
+
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
 }
 
 func addUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	var userId int
+
+	json.NewDecoder(r.Body).Decode(&user)
+	err := db.QueryRow("insert into users (name, lastname, birthdate) values($1, $2, $3) RETURNING userid;", user.Name, user.Lastname, user.Birthdate).Scan(&userId)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(userId)
 }
 
 func editUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	params := mux.Vars(r)
+
+	result, err := db.Exec("update users set name=$1, lastname=$2, birthdate=$3 where userid=$4 RETURNING userid;", user.Name, user.Lastname, user.Birthdate, params["id"])
+	logFatal(err)
+
+	rowsUpdated, err := result.RowsAffected() // how many rows were updated?
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsUpdated)
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	result, err := db.Exec("delete from users where userid=$1", params["id"])
+	logFatal(err)
+
+	rowsDeleted, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsDeleted)
 }
